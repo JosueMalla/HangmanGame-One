02c3dcd2a34b812926b0da544f7970d3a4c57048
JosueMalla <negritomallapqs@hotmail.com>
2022-09-30 22:28:04 -0500

    Adding decoration to index

---
+++ functions.js
@@ -0,0 +1,14 @@
+
+//Function for go to game zone
+
+function GoGame(){
+    window.location.href="gamezone.html";
+}
+
+function GoHome(){
+    window.location.href="index.html";
+}
+
+function GoAddword(){
+    window.location.href="addword.html";
+}
